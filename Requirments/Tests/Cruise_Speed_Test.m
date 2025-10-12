609e7753e05bbadc1f9f64eace8bb8ab7906e6f9
Andres <aospina@vt.edu>
2025-10-12 14:39:15 -0400

    Pls work this time.

--- Requirments/Tests/Cruise_Speed_Test.m
+++ Requirments/Tests/Cruise_Speed_Test.m
@@ -1,12 +1,8 @@
 classdef Cruise_Speed_Test < matlab.unittest.TestCase
-
     methods (Test)
-
         function cruise_speed_test_1(testCase)
             actualspeed = Cruise_Speed_Function();
             testCase.verifyGreaterThan(actualspeed, 3)
         end
-
     end
-
 end
